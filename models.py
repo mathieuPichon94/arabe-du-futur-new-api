8d90d27eaed6f0f5449da0c3d55f898737cf2727
Mathieu Pichon <mathieu@ubble.ai>
2022-04-17 22:27:11 +0200

    add name table

--- models.py
+++ models.py
@@ -20,6 +20,7 @@ class Result(db.Model):
 
 
 class User(db.Model):
+    __tablename__ = "results"
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(100))
