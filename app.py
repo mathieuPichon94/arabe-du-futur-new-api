e969e039da8e5e276f525410141a627f42f44805
Mathieu Pichon <mathieu@ubble.ai>
2022-04-17 22:18:53 +0200

    use config

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 
 app = Flask(__name__)
-# app.config.from_object(os.environ["APP_SETTINGS"])
+app.config.from_object(os.environ["APP_SETTINGS"])
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
